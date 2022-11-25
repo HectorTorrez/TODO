4480f10e8822127556d0ef4d2b14e1b63cebbfb9
Hector Torrez <torrezhectorb@gmail.com>
2022-11-24 20:11:33 -0600

    class todo

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles.css';
+import { Todo, TodoList } from './classes'
+
+
+const todoList = new TodoList();
+
+const tarea = new Todo('Aprender Javascript');
+
+
+todoList.nuevoTodo( tarea );
+
+console.log( todoList )
